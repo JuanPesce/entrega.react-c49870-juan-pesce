b1941abbec2b07fb84a77202dae37eaaea33c053
Juan Pesce <juan.ignacio.pesce@gmail.com>
2023-12-13 19:23:38 -0300

    avances

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -1,6 +1,7 @@
 import { addDoc, collection, getFirestore } from "firebase/firestore"
 import { useCartContext } from "../contexts/CartContext"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export const CartContainer = () =>{
 
@@ -10,21 +11,32 @@ export const CartContainer = () =>{
         email: ''
     })
 
+    const [isID, setIsID] = useState('')
     const {cartList, vaciarCarrito, totalPrecio, removeProduct} = useCartContext()
 
-    const handleOrder = () => {
+    const handleOrder = async(evt) => {
+        evt.preventDefault()
         const order = {}
-        order.buyer = {name: "juan", phone: "12345678", email: "juan@juan.com"}
+        order.buyer = formData
         order.item = cartList.map (({id, name, precio}) => ({id, name, precio}))
         order.total = totalPrecio()
-        console.log(order)
+        // console.log(order)
 
         const db = getFirestore()
         const orderCollection = collection(db, 'orders')
-
         addDoc(orderCollection, order)
-        .then(resp => console.log(resp))
+        .then(resp => setIsID(resp))
         .catch(err => console.log(err))
+        .finally(()=>{
+
+            setFormData({ 
+                name: '',
+                phone: '',
+                email: ''
+            }),
+            vaciarCarrito()
+        }
+        )
     }
 
     const handleOnChange = (evt) => {
@@ -36,15 +48,27 @@ export const CartContainer = () =>{
 
     return(
         <div>
+            {/* {isID !== '' && <>
+            <label>La orden de compra es: {isID}</label>
+            </>} */}
             {cartList.map(product => <div key={product.id}>
                                             <img className="w-25" src={product.imagen}/>
                                             Cantidad: {product.cantidad} - Precio: ${product.precio} - Subtotal: ${product.precio * product.cantidad}
                                             <button className="btn btn-danger" onClick={() => removeProduct(product.id)} > X </button>
                                         </div>
                 )}
-                <div>
+                {
+                    totalPrecio() ===0 ?
+                    <div> 
+                        El Carrito esta vacio
+                        <Link to={'/'}> Volver al menu principal </Link>
+                    </div>
+                    :
+                
+                <>
+                <label>
                     { totalPrecio() !== 0 && <label>Precio total: ${totalPrecio()} </label>}
-                </div>
+                </label><br />
             <button className="btn btn-danger" onClick={vaciarCarrito}>Vaciar!</button>
                 
             <form onSubmit={handleOrder}>
@@ -54,22 +78,25 @@ export const CartContainer = () =>{
                     name="name"
                     value={formData.name} 
                     onChange={ handleOnChange }
-                /><br /><label> Ingresar Telefono</label><br />
+                    /><br /><label> Ingresar Telefono</label><br />
                 <input 
                     type="text"
                     name="phone"
                     value={formData.phone} 
                     onChange={ handleOnChange } 
-                /><br /><label> Ingresar Email</label><br />
+                    /><br /><label> Ingresar Email</label><br />
                 <input 
                     type="text"
                     name="email"
                     value={formData.email} 
                     onChange={ handleOnChange } 
-                />
+                    />
             </form>
         
             <button className="btn btn-danger" onClick={handleOrder}>Terminar Compra</button>
+            </>
+    }
         </div>
     )
 }
+    
